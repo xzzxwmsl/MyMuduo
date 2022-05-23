ba6d61280221a5262cdd0b05d3d5e1dcf6e96b74
xzz <1365303748@qq.com>
2022-05-24 00:47:53 +0800

    noncopyable

--- base/noncopyable.h
+++ base/noncopyable.h
@@ -1,14 +1,17 @@
 #ifndef BASE_NONCOPYABLE_H
 #define BASE_NONCOPYABLE_H
 // 定义了禁止对象的拷贝
-class noncopyable
+namespace mymuduo
 {
-private:
-    noncopyable(const noncopyable &) = delete;
-    noncopyable &operator=(const noncopyable &) = delete;
+    class noncopyable
+    {
+    private:
+        noncopyable(const noncopyable &) = delete;
+        noncopyable &operator=(const noncopyable &) = delete;
 
-public:
-    noncopyable() = default;
-    ~noncopyable() = default;
-};
+    public:
+        noncopyable() = default;
+        ~noncopyable() = default;
+    };
+}
 #endif
